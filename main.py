c1fe22bcae551b768d4b0a859cb77dbb5c80b35b
manthan-kodar <63070765+manthan-kodar@users.noreply.github.com>
2021-03-16 00:47:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+######################################################################
+# Training and Evaluating
+# =======================
 from seq2seq import EncoderRNN,Linear,DecoderRNN
 from train import trainIters
 import configparser
@@ -24,24 +27,16 @@ if config["cuda"]:
 else:
     device = torch.device("cpu")
 
-#Dont edit the code
+###############################
+# Creating the dataset object #
+###############################
+# Create training data object
 bidirectional=config.getboolean("bidirectional")
-print((bidirectional)+1)
 trainset,source_vocab,target_vocab = get_dataset(types="train",batch_size=int(config["batch_size"]),shuffle=True,num_workers=int(config["num_workers"]),pin_memory=False,drop_last=True)
-print(len(target_vocab),len(source_vocab))
-
-#Encoder, decoder and bridge objects being created with parameters inputted from a config file
 encoder1 = EncoderRNN(int(config["hidden_size_encoder"]),len(source_vocab)+2,int(config["batch_size"]), num_layers=int(config["num_layer_encoder"]),bidirectional=bidirectional).to(device)
 bridge = Linear(bidirectional, int(config["hidden_size_encoder"]), int(config["hidden_size_decoder"])).to(device)
 decoder1 = DecoderRNN(int(config["hidden_size_decoder"]),len(target_vocab)+2, int(config["batch_size"]), num_layers=int(config["num_layer_decoder"])).to(device)
-
-#Calling the trainIter function.
-trainIters(trainset,encoder1, decoder1, bridge,num_epochs=10,batch_size=int(config["batch_size"]),print_every=10,device=device)
-
-#Saving the weights.
+trainIters(trainset,encoder1, decoder1, bridge,num_epochs=int(config["num_epoch"]),batch_size=int(config["batch_size"]),print_every=10,device=device)
 torch.save(encoder1,"encoder.pt")
 torch.save(decoder1,"decoder.pt")
 torch.save(bridge,"bridge.pt")
-
-
-
